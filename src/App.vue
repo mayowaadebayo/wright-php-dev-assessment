b7ff53fe51b7de8564b18a1e63f33c7442720cb5
Mayowa Adebayo <87597663+mayowaadebayo@users.noreply.github.com>
2021-07-19 08:21:52 +0100

    The assessment for Wright's web developer

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,74 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <img alt="Vue logo" src="./assets/images/logo.jpg">
+    <Home msg="Welcome to Wright Auction App"/>
+    <div>
+      <ul>
+        <li>
+         <router-link to="/">
+         Home
+        </router-link>
+        </li>
+        <li>
+         <router-link to="/">
+         Search
+        </router-link>
+        </li>
+        <li>
+         <router-link to="/">
+         About
+        </router-link>
+        </li>
+      </ul>
+      <hr/>
+      <h3>Perform a search now!</h3>
+      <form @submit.prevent="fetchProducts">
+      
+  &nbsp;
+      <select required v-model="brand">
+        <option>--Product brand--</option>
+        <option value="colourpop">Colourpop</option>
+        <option value="rejuva minerals">Rrejuva minerals</option>
+      </select>
+      &nbsp;
+      <input type="text" placeholder="Product name" v-model="product_name"/>
+      &nbsp;
+      <input type="submit" value="Search now!" name="search" required/>  
+      </form>    
+      {{Products}}
+    </div>
+    <router-view></router-view>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Home from './components/Home.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    Home
+  },
+    data () {
+    return {
+      Products: [],
+        type:'',
+        brand:'',
+        product_name:''
+    }
+   
+  },
+  mounted(){
+    // this.fetchUsers();
+  },
+  methods:{
+    fetchProducts: function () {
+      const baseURI = 'http://makeup-api.herokuapp.com/api/v1/products.json?brand='+this.brand+'&name='+this.product_name
+      this.$http.get(baseURI)
+      .then((result) => {
+        this.Products = result.data
+      })
+    }
   }
 }
 </script>
@@ -24,5 +81,16 @@ export default {
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: red;
 }
 </style>
--- src/main.js
+++ src/main.js
@@ -1,8 +1,12 @@
 import Vue from 'vue'
 import App from './App.vue'
-
-Vue.config.productionTip = false
+import router from "./router";
+import axios from 'axios';
+Vue.prototype.$http = axios
+// Vue.config.productionTip = false
 
 new Vue({
   render: h => h(App),
+  router,
+  axios
 }).$mount('#app')
